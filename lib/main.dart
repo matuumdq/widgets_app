c4ad33df2f5699a1fc4a8263f940b611404501a4
matuumdq <salinasmateo@outlook.com>
2023-08-30 11:43:19 -0300

    crear go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/router/app_router.dart';
 import 'package:widget_app/config/theme/app_theme.dart';
-import 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widget_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widget_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme(selectedColor: 4).getTheme(),
-        home: const HomeScreen(),
-        routes: {
-          '/buttons': (context) => const ButtonsScreen(),
-          '/cards': (context) => const CardsScreen(),
-        });
-    {}
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 4).getTheme(),
+      //   routes: {
+      //     '/buttons': (context) => const ButtonsScreen(),
+      //     '/cards': (context) => const CardsScreen(),
+      //   },
+    );
   }
 }
