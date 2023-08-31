819baecbb0b5136040ccb10093b25ee684c1ab17
matuumdq <salinasmateo@outlook.com>
2023-08-31 09:55:05 -0300

    add snackbars and dialogs

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,14 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Un contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'Progress Indicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'SnackBar y dialogos',
+      subTitle: 'Indicadores en pantalla',
+      link: '/snackbars',
+      icon: Icons.info_outline),
 ];
